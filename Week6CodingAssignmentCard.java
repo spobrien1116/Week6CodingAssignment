a975eec68ad4fabd7c43620b94b6fa2fd27d8047
spobrien1116 <87784429+spobrien1116@users.noreply.github.com>
2021-08-22 18:11:57 -0400

    First steps towards the final project

---
+++ Week6CodingAssignmentCard.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Week6CodingAssignmentCard {
+
+    public static void main(String[] args) {
+        //This part assigns the name of the cards
+        String[] cardNames = {"Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace"};
+        // This part sends the card names into a method (deckCreated) that will assign the card names suits, and also their values. 
+        // It then returns the completed deck of 52 cards, called createdDeck.
+        Map<String, Integer> createdDeck = (deckCreated(cardNames));
+        System.out.println(createdDeck);
+    }
+
+    // Creates the deck through a method which which assigns the suits to the cards, and also their values. Returns the completed
+    // deck of 52 cards.
+    public static Map<String, Integer> deckCreated(String[] cards) {
+        Map<String, Integer> deckOf52 = new HashMap<String, Integer>();
+        for (int i = 0; i < cards.length; i++) {
+            deckOf52.put(cards[i] + " of Spades", i + 2);
+        }
+        for (int i = 0; i < cards.length; i++) {
+            deckOf52.put(cards[i] + " of Hearts", i + 2);
+        }
+        for (int i = 0; i < cards.length; i++) {
+            deckOf52.put(cards[i] + " of Clubs", i + 2);
+        }
+        for (int i = 0; i < cards.length; i++) {
+            deckOf52.put(cards[i] + " of Diamonds", i + 2);
+        }
+        return deckOf52;
+    }
+}
---
+++ Week6CodingAssignmentDeck.java
@@ -0,0 +1,11 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class Week6CodingAssignmentDeck {
+    
+    public static void main(String[] args) {
+        Map<String, Integer> emptyDeck = new HashMap<String, Integer>();
+        System.out.println(emptyDeck);
+        System.out.println("Waiting on how to learn getters and setters.");
+    }
+}
